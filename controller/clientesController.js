9edbe06c07577af5e2021446202b12772fcfe62f
Valentina Sartorelli <valentinasartorelli@V.local>
2021-06-12 03:29:07 -0300

    Automatizo clientes

--- controller/clientesController.js
+++ controller/clientesController.js
@@ -5,12 +5,21 @@ const clientModel = jsonDB('clients');
 
 let clientesController = {
     index: (req, res) => {
-        res.render('clientes');
+        console.log("entro en index")
+        const clients = clientModel.find(req.params.id);
+        const todos = clientModel.all();
+        
+        res.render('clientes', {clients, todos});
     },
     show: (req, res) => {
+        console.log("entro en show")
         const clients = clientModel.find(req.params.id);
+        const todos = clientModel.all();
+        const id = req.params.id;
+        const url = todos[id].url;
+
         if(clients) {
-            res.render('detalleCliente', {clients});
+            res.render('detalleCliente', {clients, todos, id, url});
         } else {
             res.render('error404');
         }
